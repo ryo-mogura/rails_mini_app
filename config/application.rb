e2478782a5ad412efdcd2f772f642dcaeb4e0464
ryo-mogura <ryosuke48696869@gmail.com>
2024-04-26 14:00:37 +0900

    [chore] #4 アプリ全体のジェネレーターの設定を変更とgemの追加

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module Miniapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.test_framework false
+    end
   end
 end
+[chore] #4 アプリ全体のジェネレーターの設定を変更
