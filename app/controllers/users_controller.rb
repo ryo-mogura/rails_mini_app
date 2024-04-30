fffcf6bb5ea8564cc48811b7edc3f0d1e399eea3
ryo-mogura <ryosuke48696869@gmail.com>
2024-04-30 15:57:27 +0900

    [feat・back] #10 ユーザー登録機能、ログイン・ログアウト機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
-  def index
+  skip_before_action :require_login, only: %i[new create]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # Topページ
   root to: 'tops#index'
+
+  # ログイン
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
+  # ユーザー登録
+  resources :users, only: [:new, :create, :show, :edit, :destroy]
+
 end
